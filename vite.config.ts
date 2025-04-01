c340d0bd1252c460dba217b71f149131464fc499
Jonah Golden <jonahwgolden@gmail.com>
2025-04-01 10:44:20 -0700

    use env var port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
+	},
 });
