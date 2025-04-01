e05406a74dd8b453af04fd686018dd9da306ac35
Jonah Golden <jonahwgolden@gmail.com>
2025-04-01 11:18:42 -0700

    hardcode host

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
-		allowedHosts: [process.env.ALLOWED_HOSTS || 'localhost'],
+		allowedHosts: ['kase-world-6d61c97bfdb9.herokuapp.com', process.env.HOST || 'localhost'],
 		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
 	},
 	preview: {
-		allowedHosts: [process.env.ALLOWED_HOSTS || 'localhost'],
+		allowedHosts: ['kase-world-6d61c97bfdb9.herokuapp.com', process.env.HOST || 'localhost'],
 		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
 	},
 });
