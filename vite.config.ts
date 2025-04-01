7e74a52a8d591158a5751bcd9d4c1045cf6121bc
Jonah Golden <jonahwgolden@gmail.com>
2025-04-01 11:28:36 -0700

    Heroku setup (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		allowedHosts: ['kase-world-6d61c97bfdb9.herokuapp.com', process.env.HOST || 'localhost'],
+		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
+	},
+	preview: {
+		allowedHosts: ['kase-world-6d61c97bfdb9.herokuapp.com', process.env.HOST || 'localhost'],
+		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
+	},
 });
