2df3c466a125b75ed3f99357d10f82e774f1020e
Jonah Golden <jonahwgolden@gmail.com>
2025-04-01 11:04:52 -0700

    add to allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
+		allowedHosts: [process.env.ALLOWED_HOSTS || 'localhost'],
+		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
+	},
+	preview: {
+		allowedHosts: [process.env.ALLOWED_HOSTS || 'localhost'],
 		port: Number(process.env.PORT) || 3000, // Default to 3000 if $PORT is not set
 	},
 });
